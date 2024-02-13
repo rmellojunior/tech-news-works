7c4bee6792a4d710f4d90ad021f54c8adef2eddd
rmellojunior <renatojuniorpt@gmail.com>
2024-02-13 17:15:34 +0000

    added custom dimensions

---
+++ app/src/main/java/com/example/technewsworks/ui/theme/Dimens.kt
@@ -0,0 +1,24 @@
+package com.example.technewsworks.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val height: Dp = 100.dp,
+    val padding: Dp = 12.dp,
+
+    // card
+    val cardElevation: Dp = 10.dp,
+    val cardSpaceBy: Dp = 8.dp,
+)
+
+val LocalDimensions = staticCompositionLocalOf { Dimensions() }
+
+val MaterialTheme.pDimensions
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalDimensions.current
