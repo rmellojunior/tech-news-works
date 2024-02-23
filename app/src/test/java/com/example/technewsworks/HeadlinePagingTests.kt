6a59ae0739aee14bb882f579aedd41f4e07f13bd
rmellojunior <renatojuniorpt@gmail.com>
2024-02-23 10:42:13 +0000

    paging tests (2)

--- app/src/test/java/com/example/technewsworks/HeadlinePagingTests.kt
+++ app/src/test/java/com/example/technewsworks/HeadlinePagingTests.kt
@@ -3,6 +3,7 @@ package com.example.technewsworks
 import androidx.paging.PagingSource
 import com.example.technewsworks.data.datasource.api.HeadlinesApiService
 import com.example.technewsworks.data.datasource.mock.FakeNews
+import com.example.technewsworks.data.models.Article
 import com.example.technewsworks.data.models.NewsApiResponse
 import com.example.technewsworks.data.repo.headlines.HeadlinesPagingSource
 import io.mockk.coEvery
@@ -60,7 +61,36 @@ class HeadlineTests {
 
     @Test
     fun `fetch top headlines using paging - error`() = runTest {
+        // arrange
+        val loadSize = 2
 
+        val headlinesApiService = mockk<HeadlinesApiService>()
+        coEvery {
+            headlinesApiService.getTopHeadlines(any(), any(), any())
+        } returns Response.error(401, mockk(relaxed = true))
+
+        val pagingSource = HeadlinesPagingSource(
+            apiService = headlinesApiService
+        )
+
+        val params = PagingSource
+            .LoadParams
+            .Refresh(
+                key = 1, // page
+                loadSize = loadSize,
+                placeholdersEnabled = false
+            )
+
+        val expected = PagingSource
+            .LoadResult
+            .Error<Int, Article>(
+                throwable = Throwable()
+            )::class.java
+
+        // act
+        val actual = pagingSource.load(params = params)::class.java
+
+        // assert
+        assertEquals(expected, actual)
     }
-
 }
